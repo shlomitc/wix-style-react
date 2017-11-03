5468b2ce0dcd56356407933a93e018b5d4788866
Lior Belinsky <liorbe@wix.com>
2017-11-03 17:48:41 +0200

    not relevant fix

--- stories/Backoffice/Button/index.js
+++ stories/Backoffice/Button/index.js
@@ -1,40 +1,40 @@
-  import React from 'react';
-  import CodeExample from '../../utils/Components/CodeExample';
+import React from 'react';
+import CodeExample from '../../utils/Components/CodeExample';
 
-  import ExampleControlled from './ExampleControlled';
-  import ExampleControlledRaw from '!raw-loader!./ExampleControlled';
-  import IconsExample from './ExampleWithIcons';
-  import IconsExampleRaw from '!raw-loader!./ExampleWithIcons';
+import ExampleControlled from './ExampleControlled';
+import ExampleControlledRaw from '!raw-loader!./ExampleControlled';
+import IconsExample from './ExampleWithIcons';
+import IconsExampleRaw from '!raw-loader!./ExampleWithIcons';
 
-  import story from '../../utils/Components/Story';
-  import * as Icons from 'wix-style-react/Icons';
+import story from '../../utils/Components/Story';
+import * as Icons from 'wix-style-react/Icons';
 
-  const icons = Object.values(Icons).map(icon => React.createElement(icon));
+const icons = Object.values(Icons).map(icon => React.createElement(icon));
 
-  story({
-    category: 'Backoffice',
-    storyName: 'Button',
-    componentSrcFolder: 'Backoffice/Button',
-    componentProps: {
-      disabled: false,
-      theme: 'fullblue',
-      children: 'Click Me'
-    },
-    exampleProps: {
-      onClick: () => 'Clicked!',
-      onMouseEnter: () => 'Mouse Enter!',
-      onMouseLeave: () => 'Mouse Leave!',
-      prefixIcon: icons,
-      suffixIcon: icons
-    },
-    examples: (
-      <div>
-        <CodeExample title="With icons" code={IconsExampleRaw}>
-          <IconsExample/>
-        </CodeExample>
-        <CodeExample title="Controlled" code={ExampleControlledRaw}>
-          <ExampleControlled/>
-        </CodeExample>
-      </div>
-    )
-  });
+story({
+  category: 'Backoffice',
+  storyName: 'Button',
+  componentSrcFolder: 'Backoffice/Button',
+  componentProps: {
+    disabled: false,
+    theme: 'fullblue',
+    children: 'Click Me'
+  },
+  exampleProps: {
+    onClick: () => 'Clicked!',
+    onMouseEnter: () => 'Mouse Enter!',
+    onMouseLeave: () => 'Mouse Leave!',
+    prefixIcon: icons,
+    suffixIcon: icons
+  },
+  examples: (
+    <div>
+      <CodeExample title="With icons" code={IconsExampleRaw}>
+        <IconsExample/>
+      </CodeExample>
+      <CodeExample title="Controlled" code={ExampleControlledRaw}>
+        <ExampleControlled/>
+      </CodeExample>
+    </div>
+  )
+});
