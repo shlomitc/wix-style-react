38d5ac2fa3f9d2fa0db9072efb3655345ffae33b
Boris Korneev <borisk@wix.com>
2017-12-19 05:42:35 +0200

    refactor(Loader): reuse size defined in JS

--- src/Loader/Loader.js
+++ src/Loader/Loader.js
@@ -55,7 +55,12 @@ export default class Loader extends WixComponent {
 
     return (
       <div className={classNames(css.loaderContainer, css[size], css[color])}>
-        <div className={css.arcsContainer}>
+        <div
+          className={css.arcsContainer}
+          style={{
+            width: `${sizeInPx}px`,
+            height: `${sizeInPx}px`}}
+          >
           <Arc
             angle={lightArcAngle}
             className={css.lightArc}
