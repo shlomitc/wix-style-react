0bb0f3aa919d60f1f8095251062fc3cea0fb202e
Boris Korneev <borisk@wix.com>
2017-12-19 05:32:19 +0200

    refactor(Search): move default placeholder to `defaultProps`

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -17,7 +17,8 @@ export default class Search extends WixComponent {
   };
 
   static defaultProps = {
-    ...InputWithOptions.defaultProps
+    ...InputWithOptions.defaultProps,
+    placeholder: 'Search'
   };
 
   constructor(props) {
@@ -69,7 +70,6 @@ export default class Search extends WixComponent {
   render() {
     return (
       <InputWithOptions
-        placeholder="Search"
         {...this.props}
         ref="searchInput"
         roundInput
